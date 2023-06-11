65d6d7b5ab6f72e319082cb596ed84119112d60d
tornikee <tornikexoxiashvili@gmail.com>
2023-06-11 14:16:14 +0400

    change

--- src/main/java/com/epam/rd/autotasks/suffixing/Main.java
+++ src/main/java/com/epam/rd/autotasks/suffixing/Main.java
@@ -13,6 +13,7 @@ import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getName());
 
