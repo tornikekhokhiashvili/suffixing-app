f8e96832378a8a0c243869c11debaaeb5420c2fc
tornikee <tornikexoxiashvili@gmail.com>
2023-06-11 14:02:38 +0400

    change

--- src/main/java/com/epam/rd/autotasks/suffixing/Main.java
+++ src/main/java/com/epam/rd/autotasks/suffixing/Main.java
@@ -1,9 +1,14 @@
 package com.epam.rd.autotasks.suffixing;
 
+import org.opentest4j.AssertionFailedError;
+
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -39,7 +44,7 @@ public class Main {
         }
 
         if (suffix == null || suffix.isEmpty()) {
-            logger.log(Level.SEVERE, "No suffix is specified in the config.");
+            logger.log(Level.SEVERE, "No suffix is configured");
             return;
         }
 
@@ -57,23 +62,47 @@ public class Main {
 
     private static void processFile(String filePath, String mode, String suffix) {
         String destinationFilePath = addSuffixToFile(filePath, suffix);
+        boolean sourceFileExists = Files.exists(Paths.get(filePath));
 
-        if (destinationFilePath == null) {
+        if (!sourceFileExists) {
             logger.log(Level.SEVERE, "No such file: " + filePath.replace('\\', '/'));
             return;
         }
+        String sourcePath = filePath.replace('\\', '/');
+        String destinationPath = destinationFilePath.replace('\\', '/');
 
         switch (mode.toLowerCase()) {
             case "copy":
-                logger.log(Level.INFO, filePath.replace('\\', '/') + " -> " + destinationFilePath.replace('\\', '/'));
-                // Copy the file to the destinationFilePath
+                try {
+                    Path source = Path.of(sourcePath);
+                    Path destination = Path.of(destinationPath);
+                    Files.copy(source, destination, StandardCopyOption.COPY_ATTRIBUTES);
+                    logger.log(Level.INFO, sourcePath + " -> " + destinationPath);
+                } catch (IOException e) {
+                    logger.log(Level.SEVERE, "Error copying file: " + e.getMessage());
+                }
                 break;
             case "move":
-                logger.log(Level.INFO, filePath.replace('\\', '/') + " => " + destinationFilePath.replace('\\', '/'));
-                // Move the file to the destinationFilePath
+                try {
+                    Path source = Path.of(sourcePath);
+                    Path destination = Path.of(destinationPath);
+                    Files.move(source, destination, StandardCopyOption.REPLACE_EXISTING);
+                    logger.log(Level.INFO, sourcePath + " => " + destinationPath);
+                } catch (IOException e) {
+                    logger.log(Level.SEVERE, "Error moving file: " + e.getMessage());
+                }
                 break;
             default:
                 logger.log(Level.SEVERE, "Mode is not recognized: " + mode);
+                return; // Stop processing if the mode is not recognized
+        }
+
+
+        // Check if the destination file exists
+        File destinationFile = new File(destinationFilePath);
+        if (!destinationFile.exists()) {
+            logger.log(Level.SEVERE, "File " + destinationPath + " must exist");
+
         }
     }
     private static String addSuffixToFile(String filePath, String suffix) {
